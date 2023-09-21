40672dd17cd21b57395682e7ccb932918533fdaa
유채우 <rina0405@naver.com>
2023-09-21 13:40:40 +0900

    #24 SessionUser DTO에 직렬화 기능 추가

--- freshgreenish/src/main/java/com/sidenow/freshgreenish/global/config/auth/dto/SessionUser.java
+++ freshgreenish/src/main/java/com/sidenow/freshgreenish/global/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.sidenow.freshgreenish.global.config.auth.dto;
 import com.sidenow.freshgreenish.domain.user.entity.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String nickname;
     private String email;
     private String picture;
