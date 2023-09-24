d0ad1c3b0f716d890faffa62f6ceed589b087f67
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2023-09-24 15:02:11 +0900

    문의 답변 기능 구현 (#29)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

--- freshgreenish/src/main/java/com/sidenow/freshgreenish/domain/answer/repository/CustomAnswerRepository.java
+++ freshgreenish/src/main/java/com/sidenow/freshgreenish/domain/answer/repository/CustomAnswerRepository.java
@@ -1,4 +1,7 @@
 package com.sidenow.freshgreenish.domain.answer.repository;
 
+import com.sidenow.freshgreenish.domain.answer.dto.GetAnswerDetail;
+
 public interface CustomAnswerRepository {
+    GetAnswerDetail getAnswerDetail(Long questionId);
 }
