804e755a3d8cc3e8ea4031843492777a74c5700a
유채우 <rina0405@naver.com>
2023-10-24 23:57:26 +0900

    fix: 관리자 ROLE 추가

--- freshgreenish/src/main/java/com/sidenow/freshgreenish/domain/user/repository/UserRepository.java
+++ freshgreenish/src/main/java/com/sidenow/freshgreenish/domain/user/repository/UserRepository.java
@@ -1,18 +1,12 @@
 package com.sidenow.freshgreenish.domain.user.repository;
 
-import com.sidenow.freshgreenish.domain.delivery.entity.Delivery;
-import com.sidenow.freshgreenish.domain.purchase.entity.Purchase;
-import com.sidenow.freshgreenish.domain.purchase.entity.SubscriptionStatus;
 import com.sidenow.freshgreenish.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByNickname(String nickname);
-//    List<Purchase> findAllByUserIdAndSubscriptionStatus(Long userId, SubscriptionStatus subscriptionStatus);
-//    Delivery findByPurchaseId(Long purchaseId);
 }
